16773c51dfabcc67194a0df634d3519b835c1ae5
Avishek De <avishekde.iitr@gmail.com>
2022-11-03 00:09:30 -0700

    modifies routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
+  get '/orders', to: 'orders#index'
+  get '/ratings/:order_id', to: 'ratings#new', as: 'new_rating'
+  post '/ratings/:order_id', to: 'ratings#create'
 end
