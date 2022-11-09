994a198dad4cc91040a7ae3cc77293b4e8d7ec79
Avishek De <avishekde.iitr@gmail.com>
2022-11-08 20:53:11 -0800

    updated homepage layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'home#index'
   get '/orders', to: 'orders#index'
   get '/ratings/:order_id', to: 'ratings#new', as: 'new_rating'
   get '/items/new', to: 'items#new'
