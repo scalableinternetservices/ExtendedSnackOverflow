cd7e6b7455cc53197a6cd7efff5f11ba8f7ec7c4
Ajit Jadhav <ajit.171198@gmail.com>
2022-11-12 21:45:52 -0800

    Add: Item association with seller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_13_015631) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_054237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_015631) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "seller_id"
+    t.string "integer"
   end
 
   create_table "orders", force: :cascade do |t|
