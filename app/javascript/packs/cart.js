074417194274d1c84c12ddcba6317eaf2efe8c81
Avishek De <avishekde.iitr@gmail.com>
2022-11-12 18:02:56 -0800

    associating buyer with address

--- app/javascript/packs/cart.js
+++ app/javascript/packs/cart.js
@@ -1,16 +1,18 @@
+var removedItems = [];
+
 window.addQuantity = function () {
     alert("add quantity");
 }
 
 window.changeQuantity = function (selectElement) {
     const id = selectElement.id.split('_');
-    var costField = document.getElementById("cost_"+id[1]);
-    var priceField = document.getElementById("price_"+id[1]);
+    var costField = document.getElementById("cost_" + id[1]);
+    var priceField = document.getElementById("price_" + id[1]);
     var totalField = document.getElementById("totalCost");
-    
+
     var prevCost = parseInt(costField.innerText.split("$")[1]);
     var currTotal = parseInt(totalField.innerText.split("$")[1]);
-    
+
     var newCost = priceField.innerText.split("$")[1] * selectElement.value
 
     costField.innerHTML = "<b>" + costField.innerText.split("$")[0] + "$" + newCost + "</b>";
@@ -25,11 +27,39 @@ window.placeOrder = function (value) {
 
     var requestArray = [];
     var ids = value.split(',');
-    for(id of ids) {
-        var quant = document.getElementById("quantity_"+id).value;
-        var obj = {"item_id": id, "quantity": quant};
+    ids = ids.filter(function (id) {
+        return !removedItems.includes(id);
+    });
+    for (id of ids) {
+        var quant = document.getElementById("quantity_" + id).value;
+        var obj = { "item_id": id, "quantity": quant };
         requestArray.push(obj);
     }
-    document.getElementById("requestparam").value = JSON.stringify({body: requestArray});
+    document.getElementById("requestparam").value = JSON.stringify({ body: requestArray });
+    removedItems = [];
 
+}
+
+window.removeItem = function (e, deleteItem) {
+    e.preventDefault();
+    const id = deleteItem.id.split('_');
+    removedItems.push(id[1]);
+
+    var costField = document.getElementById("cost_" + id[1]);
+    var totalField = document.getElementById("totalCost");
+
+    var prevCost = parseInt(costField.innerText.split("$")[1]);
+    var currTotal = parseInt(totalField.innerText.split("$")[1]);
+
+    var newTotal = currTotal - prevCost;
+
+    totalField.innerHTML = "<b>" + totalField.innerText.split("$")[0] + "$" + newTotal + "</b>";
+
+    document.getElementById("item_" + id[1]).remove();
+
+    if (newTotal == 0) {
+        document.getElementById("address_div").remove();
+        document.getElementById("place_order").addClass("disabled");
+    }
+    e.preventDefault();
 }
