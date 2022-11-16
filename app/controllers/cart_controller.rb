70f73f8f34ebf2b63ed69fb1018dd60c50ac2eab
deept368 <dipt1995@gmail.com>
2022-11-16 00:42:03 -0800

    Changed CSRF protection flow

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,55 +1,54 @@
 class CartController < ApplicationController
 	before_action :authenticate_buyer!
-	skip_before_action :verify_authenticity_token, only: [:additem]
-	
+
 	def showcart
-  
-		  items = Cart.where(buyer_id: current_buyer.id).order(:id)
-		  @carts = items
-		  
-		  price = 0;
-		  items.each do |cart|
+
+		items = Cart.where(buyer_id: current_buyer.id).order(:id)
+		@carts = items
+
+		price = 0;
+		items.each do |cart|
 			price += cart.item.price
-		  end
-		  @totalcost = price
-		  render 'cart/showcart'
+		end
+		@totalcost = price
+		render 'cart/showcart'
 	end
-  
+
 	def placeorder
-		  item_ids = JSON.parse(params[:requestparam])
-		  ActiveRecord::Base.transaction do
+		item_ids = JSON.parse(params[:requestparam])
+		ActiveRecord::Base.transaction do
 			item_ids["body"].each do |item|
-			  Order.create(:item_id => item['item_id'], :quantity => item['quantity'], 
-					  :buyer_id => current_buyer.id)
+				Order.create(:item_id => item['item_id'], :quantity => item['quantity'],
+										 :buyer_id => current_buyer.id)
 			end
 			Cart.delete_all
-		  end
-		  render 'cart/placeorder'
+		end
+		render 'cart/placeorder'
 	end
-  
+
 	def additem
 		item_id = params[:id]
-  
+
 		buyer_id = current_buyer.id
-  
+
 		if Cart.find_by_item_id(item_id)
 			redirect_to root_path, notice: "Item already exists in the Cart"
 			return
 		end
 		@cart = Cart.new(item_id: item_id, buyer_id: buyer_id)
 		if @cart.save
-			  redirect_to root_path, notice: "Item succesfully added to the Cart"
-		  else
-			  redirect_to root_path, notice: "Something went wrong"
-		  end
+			redirect_to root_path, notice: "Item succesfully added to the Cart"
+		else
+			redirect_to root_path, notice: "Something went wrong"
+		end
 	end
-  
+
 	def deleteitem
-	  item_id = params[:id]
-	  if Cart.find_by_item_id(item_id)
-		  Cart.find_by_item_id(item_id).destroy
-		  item = Item.find(item_id)
-		  redirect_to showcart_path, notice: item.name+ " removed from the Cart"
-	  end
+		item_id = params[:id]
+		if Cart.find_by_item_id(item_id)
+			Cart.find_by_item_id(item_id).destroy
+			item = Item.find(item_id)
+			redirect_to showcart_path, notice: item.name+ " removed from the Cart"
+		end
 	end
-  end
+end
