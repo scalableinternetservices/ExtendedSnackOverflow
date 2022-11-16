65ac198e37472d6ecc7fdadaa6fad532e6425e96
Avishek De <avishekde.iitr@gmail.com>
2022-11-16 00:55:53 -0800

    added query optimization

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,12 +6,13 @@ class ItemsController < ApplicationController
 	end
 
 	def show
+		# After changing out Item model to include ratings, N+1 Query optimization is not needed here.
 		@item = Item.find(params[:id])
-		@ratings = @item.ratings.paginate(page: params[:page], per_page: 10)
+		@ratings = @item.ratings
 		# if more than one stars awarded, calculate avg
 		# else give a default 0 rating
 		@overall_rating = @ratings.count > 0 ? @ratings.sum(:stars).to_f / @ratings.count : 0.0
-		@overall_rating = @overall_rating.round(1) # Rounding it to 1 decimal point
+		@overall_rating = @overall_rating.round(1) # Rounding it to 1 decimal point)
 	end
 
 	def create
@@ -24,8 +25,8 @@ class ItemsController < ApplicationController
 		end
 	end
 	private
-	def item_params
-		params.require(:item).permit(:name, :description, :price)
-	end
+		def item_params
+			params.require(:item).permit(:name, :description, :price)
+		end
 
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
 	before_action :authenticate_buyer!
-	
+
 	def index
-		@orders = Order.where(buyer_id: current_buyer.id).paginate(page: params[:page], per_page: 10)
+		@orders = Order.where(buyer_id: current_buyer.id).includes(:item).includes(:rating).reverse # Needed to show latest orders first
 	end
 end
