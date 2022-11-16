d5c09956168396658a43db14e40b40c455706c3a
apoo <apoo@ResNet-24-163.resnet.ucsb.edu>
2022-11-16 00:19:09 -0800

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_13_062243) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_055214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "buyers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,7 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_062243) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "seller_id"
-    t.string "integer"
   end
 
   create_table "orders", force: :cascade do |t|
