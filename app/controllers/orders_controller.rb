82d3ad7be20333461b3c54c59a788d55ceb14197
deept368 <dipt1995@gmail.com>
2022-11-16 00:49:18 -0800

    Added pagination

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,6 @@ class OrdersController < ApplicationController
 	before_action :authenticate_buyer!
 	
 	def index
-		@orders = Order.where(buyer_id: current_buyer.id).reverse
+		@orders = Order.where(buyer_id: current_buyer.id).paginate(page: params[:page], per_page: 10)
 	end
 end
