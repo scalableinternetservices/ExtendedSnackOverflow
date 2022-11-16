d243c16ee70834dae0f4fa4265b07b6e597bbb6e
Avishek De <avishekde.iitr@gmail.com>
2022-11-16 00:10:09 -0800

    removes integer from items migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_13_062243) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_080726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_062243) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "seller_id"
-    t.string "integer"
   end
 
   create_table "orders", force: :cascade do |t|
